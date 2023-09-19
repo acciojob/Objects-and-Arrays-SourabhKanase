fe14d40b43c5853b183b4beeedc97d591d71dcbd
SourabhKanase <141937826+SourabhKanase@users.noreply.github.com>
2023-09-19 09:11:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 const players = ["John", "Bob", "Alice", "Poppy"];
-
 const person = {
   name: "John Doe",
   age: 80,
 };
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+// Create a reference to the players array
+const team = players;
 
+// Create a shallow copy of the players array
+const team1 = [...players];
+
+// Create a shallow copy of the person object
+const cap1 = Object.assign({}, person);
+
+// Assign the variables to the window object
 window.players = players;
 window.person = person;
 window.team = team;
 window.team1 = team1;
 window.cap1 = cap1;
+
+// Test the copies
+console.log("Original team:", team);
+console.log("Copy team1:", team1);
+console.log("Copy cap1:", cap1);
+
+// Modifying the original array and object to test edge cases
+players.push("Sarah");
+person.name = "Jane Smith";
+
+console.log("Modified original team:", team);
+console.log("Modified copy team1:", team1);
+console.log("Modified copy cap1:", cap1);
